74c4afd70cdfaf4acff0a52459d5a60749961ccf
HAlGhanim <imzaji98@gmail.com>
2023-06-19 19:54:48 +0300

    done with the task will do the bonus later

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-const express = require('express');
-const app = express();
-const postsRoutes = require('./api/posts/posts.routes');
-const connectDb = require('./database');
+const express = require("express");
+const postsRoutes = require("./api/posts/posts.routes");
+const connectDb = require("./database");
+const notFoundHandler = require("./middlewares/notFoundHandler");
+const errorHandler = require("./middlewares/errorHandler");
 
 connectDb();
+const app = express();
+
 app.use(express.json());
-app.use('/posts', postsRoutes);
+app.use("/posts", postsRoutes);
+
+app.use(notFoundHandler);
+app.use(errorHandler);
 
 app.listen(8000, () => {
-  console.log('The application is running on localhost:8000');
+  console.log("The application is running on localhost:8000");
 });
